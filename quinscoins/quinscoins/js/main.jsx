00af1ef643bd38f3b0c5494fb3cd71b5c1ad8873
Ryan <rryanq@umich.edu>
2018-05-04 13:46:15 -0400

    adding javascript functionality

---
+++ quinscoins/quinscoins/js/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Term_search from './term_search';
+
+ReactDOM.render(
+  <Term_search />,
+  document.getElementById('reactFeedEntry'),
+);
